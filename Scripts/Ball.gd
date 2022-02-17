0367bbd60ebc681e027ac71120ba5bd6ef3e364a
alexanderhofmann <kleineinsteinalex@gmail.com>
2022-02-17 15:12:32 +0100

    Added bounce off cealing

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,17 +1,22 @@
 extends RigidBody2D
 
-export var velocity: Vector2 = Vector2(10.0, 0.0)
+var velocity: Vector2 = Vector2(10.0, 0.0)
+export var speed: float = 10.0
 export var reflectFactor: float = 0.1
 var methodToCall: String = ""
 
 func _ready():
-	linear_velocity = velocity
+	linear_velocity = velocity.normalized() * speed
 	
 func _physics_process(delta):
-	linear_velocity = velocity
+	linear_velocity = velocity.normalized() * speed
 	if methodToCall != "":
 		call(methodToCall)
 	
 func switchDirection():
 	velocity = velocity.reflect(Vector2.UP)
 	methodToCall = ""
+
+func _on_screen_exited():
+	print("Screen exited")
+	velocity = velocity.bounce(Vector2.UP)
