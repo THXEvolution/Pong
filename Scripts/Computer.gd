78c2761f7e83e30bdf6637bb2443b12feb4252c8
Alex <kleineinsteinalex@gmail.com>
2022-02-27 21:17:09 +0100

    added better ball control and computer advancements

--- Scripts/Computer.gd
+++ Scripts/Computer.gd
@@ -1,8 +1,11 @@
-extends Area2D
+extends KinematicBody2D
 
 signal ballEntered
 
 var moving: bool = true
+var velocity: Vector2 = Vector2.ZERO setget setVelocity
+export var maxSpeed: float = 150
+var speed: float = 20 setget setSpeed
 
 func _on_Computer_body_entered(body):
 	moving = false
@@ -11,3 +14,11 @@ func _on_Computer_body_entered(body):
 
 func _on_Computer_body_exited(body):
 	moving = true
+
+func setVelocity(value):
+	velocity = value.normalized() * speed
+	move_and_slide(velocity)
+
+func setSpeed(value):
+	speed = clamp(value, -maxSpeed, maxSpeed)
+	self.velocity = velocity.normalized() * speed
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,7 +3,7 @@ extends Node
 var playerScore: int = 0 setget setPlayerScore
 var computerScore: int = 0 setget setComputerScore
 var playerScored: bool = true
-var maxScore: int = 1
+var maxScore: int = 11
 var ball = load("res://Scenes/Ball.tscn").instance()
 var _playerScore: int = 0
 var _computerScore: int = 0
@@ -14,16 +14,18 @@ func _ready():
 		playerScored = false
 
 func _physics_process(delta):
-	#if $Computer.moving and $Ball != null:
-		#$Computer.position.y = $Ball.position.y
-	pass
+	$Computer.velocity = Vector2.DOWN
+	$Computer.speed = $Ball.velocity.y
+	
+	$Computer.position.y = clamp($Computer.position.y, 32, 567)
+	$Player.position.y = clamp($Player.position.y, 32, 567)
 
 func _on_ballEntered(which):
 	var delta: float = 0.0
 	if which == "Player":
-		delta = ($Ball.position.y - $Player.position.y) * $Ball.reflectFactor
+		delta = clamp(($Ball.position.y - $Player.position.y) * $Ball.reflectFactor, -$Ball.speed, $Ball.speed)
 	elif which == "Computer":
-		delta = ($Ball.position.y - $Computer.position.y) * $Ball.reflectFactor
+		delta = clamp(($Ball.position.y - $Computer.position.y) * $Ball.reflectFactor, -$Ball.speed, $Ball.speed)
 	$Ball.velocity.y = delta
 	$Ball.methodToCall = "switchDirection"
 
